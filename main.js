a507a77541d0475e2e5a4442c271dceeba79284b
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2020-01-09 13:15:05 -0300

    classes, herança e métodos estáticos

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
-class Teste {
-    
+//classes, herança e metodos estáticos
+
+class List {
+    constructor(){
+        this.data = [];
+    }
+
+    add(data) {
+        this.data.push(data);
+        console.log(this.data);
+    }
 }
+
+class TodoList extends List {
+    static soma(a, b){
+        return a + b;
+    }
+}
+
+const MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick = function(){
+    MinhaLista.add('Novo todo');
+    console.log(TodoList.soma(1, 1));
+}
+
