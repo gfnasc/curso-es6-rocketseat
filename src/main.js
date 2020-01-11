3fda99773cb4d95ed96b4dbad5bb826fde65eef4
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2020-01-11 17:36:46 -0200

    aplicação final do curso

--- src/main.js
+++ src/main.js
@@ -1,11 +1,72 @@
-import axios from 'axios';
+import api from './api';
 
-class Api {
-    static async getUserInfo(username){
-        const response = await axios.get(`https://api.github.com/users/${username}`);
+class App {
+    constructor(){
+        this.repositories = [];
 
-        console.log(response);
+        this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]');
+        this.listEl = document.getElementById('repo-list');
+        
+        this.registerHandlers();
     }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    async addRepository(event) {
+        event.preventDefault();
+
+        const repoInput = this.inputEl.value;
+
+        if(repoInput.length === 0)
+            return;
+
+        const response = await api.get(`/repos/${repoInput}`);
+        console.log(response);
+        console.log(this.repositories);
+        const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+        this.repositories.push({
+            name,
+            description,
+            avatar_url,
+            html_url
+        });
+
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        console.log(this.repositories);
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+        
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
+    }
+
 }
 
-Api.getUserInfo('gfnasc');
+new App();
